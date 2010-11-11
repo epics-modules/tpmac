6733541e45c86ce0cfd89c5c9aab96aad29b6af8
Jonathan Thompson <Jonathan.Thompson@diamond.ac.uk>
2010-11-11 16:10:51 +0000

    tpmac: Updated motor dependancy to latest.  Added pmac simulations.

---
+++ etc/simulations/PmacUi.py
@@ -0,0 +1,208 @@
+#!/bin/env python2.4
+
+import curses, readline
+
+def getUi():
+    if PmacUi.ui is None:
+        PmacUi(True)  # No UI made yet, create one with no console
+    return PmacUi.ui
+
+class PmacUi(object):
+    ui = None
+
+    def __init__(self, noConsole):
+        PmacUi.ui = self
+        self.noConsole = noConsole
+        if not self.noConsole:
+            # Initialise the library and partition the screen
+            self.wholeScr = curses.initscr()
+            self.partitionScreen()
+        # Initialise the source display variables
+        self.fileName = ''
+        self.fileContents = []
+        self.lineNumber = -1
+        self.firstDisplayLine = 0
+        # Initialise the output window variables
+        self.outputX = 0
+        self.outputY = 0
+        self.outputLines = ['']
+        self.outputNewLine = False
+        # Initialise the watch window variables
+        self.watchVars = {}
+        self.activeMachine = None
+        # Initialise the trace log
+        self.traceLogOn = False
+        self.traceFileName = ''
+
+    def setTraceLog(self, fileName):
+        self.traceLogOn = True
+        self.traceFileName = fileName
+
+    def partitionScreen(self):
+        sizeY, sizeX = self.wholeScr.getmaxyx()
+        self.commandWindow = curses.newwin(1,sizeX, sizeY-1,0)
+        self.outputWindow = curses.newwin(10,sizeX, sizeY-11,0)
+        self.watchWindow = curses.newwin(sizeY-12,20, 0,0)
+        self.sourceWindow = curses.newwin(sizeY-12,sizeX-21, 0,21)
+        self.wholeScr.hline(sizeY-12,0, curses.ACS_HLINE, sizeX)
+        self.wholeScr.vline(0,20, curses.ACS_VLINE, sizeY-11)
+        self.wholeScr.refresh()
+        self.outputWindow.idlok(1)
+
+    def cleanUp(self):
+        if not self.noConsole:
+            curses.endwin()
+
+    def displayLine(self, fileName, lineNumber):
+        sizeY, sizeX = self.sourceWindow.getmaxyx()
+        if self.fileName != fileName:
+            # Read in the new file
+            self.debug("Reading file %s" % fileName)
+            self.fileName = fileName
+            self.fileContents = []
+            try:
+                file = open(self.fileName, "r")
+            except IOError:
+                self.output("Cannot open file %s\n" % fileName)
+            else:
+                for line in file:
+                    line = line.expandtabs()
+                    line = line[0:sizeX-1].rstrip()
+                    self.fileContents.append(line)
+            self.fileName = fileName
+            self.lineNumber = -1
+        if len(self.fileContents) > 0 and self.lineNumber != lineNumber and \
+                lineNumber <= len(self.fileContents):
+            if False: #lineNumber >= self.firstDisplayLine and \
+                #lineNumber < (self.firstDisplayLine + sizeY - 2):
+                # The line is on the screen, just move the highlight
+                self.sourceWindow.addstr(self.lineNumber-self.firstDisplayLine+1,1, 
+                    self.fileContents[self.lineNumber], curses.A_NORMAL)
+                self.sourceWindow.addstr(lineNumber-self.firstDisplayLine+1,1, 
+                    self.fileContents[lineNumber], curses.A_BOLD)
+            else:
+                # Display the current file contents, centered on the line number
+                self.firstDisplayLine = lineNumber - sizeY/2
+                if self.firstDisplayLine < 0:
+                    self.firstDisplayLine = 0
+                for i in range(sizeY):
+                    displayLine = self.firstDisplayLine + i
+                    displayText = ''
+                    if (displayLine-1) < len(self.fileContents):
+                        displayText = self.fileContents[displayLine-1]
+                    self.sourceWindow.hline(i,0, ' ', sizeX)
+                    if len(displayText) > 0:
+                        attr = curses.A_NORMAL
+                        if displayLine == lineNumber:
+                            attr = curses.A_BOLD
+                        try:
+                            self.sourceWindow.addnstr(i,0, displayText, sizeX, attr)
+                        except:
+                            getUi().output("addstr error [%s]\n" % repr(displayText))
+            self.lineNumber = lineNumber
+            self.sourceWindow.refresh()
+            # Output to the log file
+            self.outputToTraceLog('%s:%s %s\n' % (fileName, lineNumber,
+                self.fileContents[lineNumber-1]))
+            self.debug('Line %s: %s\n' % (lineNumber, self.fileContents[lineNumber-1]))
+
+    def outputToTraceLog(self, text):
+        if self.traceLogOn:
+            logFile = open(self.traceFileName, 'a')
+            logFile.write(text)
+
+    def debug(self, text):
+        logFile = open('debug.txt', 'a')
+        logFile.write(text)
+
+    def output(self, text):
+        if self.noConsole:
+            print text
+        else:
+            maxY, maxX = self.outputWindow.getmaxyx()
+            for ch in text:
+                if self.outputNewLine:
+                    self.outputLines.append('')
+                    self.outputY += 1
+                    self.outputX = 0
+                    if self.outputY >= maxY:
+                        self.outputLines = self.outputLines[1:]
+                        for line in range(maxY):
+                            self.outputWindow.hline(line,0, ' ', maxX)
+                            self.outputWindow.addstr(line,0, self.outputLines[line])
+                        self.outputY = maxY-1
+                    self.outputNewLine = False
+                if ch == '\n':
+                    self.outputNewLine = True
+                else:
+                    self.outputLines[-1] = self.outputLines[-1] + ch
+                    try:
+                        self.outputWindow.addch(self.outputY, self.outputX, ch)
+                    except:
+                        print "addch error: %s,%s, %s, %s,%s" % (self.outputY, self.outputX, 
+                            ch, maxY, maxX)
+                    self.outputX += 1
+                    if self.outputX >= (maxX-1):
+                        self.outputNewLine = True
+            self.outputWindow.refresh()
+        
+    def inputLine(self, prompt):
+        maxY, maxX = self.commandWindow.getmaxyx()
+        self.commandWindow.hline(0,0, ' ', maxX)
+        self.commandWindow.addstr(0,0, prompt)
+        self.commandWindow.refresh()
+        return self.commandWindow.getstr()
+
+    def watchVar(self, machine, var, val):
+        if machine == self.activeMachine:
+            if var not in self.watchVars:
+                # Allocate a line for this watch variable
+                self.watchVars[var] = len(self.watchVars)
+            self.updateVar(machine, var, val)
+
+    def updateVar(self, machine, var, val):
+        if machine == self.activeMachine:
+            if var in self.watchVars:
+                # Write the watch line
+                maxY, maxX = self.watchWindow.getmaxyx()
+                line = self.watchVars[var]
+                self.watchWindow.hline(line,0, ' ', maxX)
+                self.watchWindow.addstr(line,0, '%s=%s' % (var, val))
+                self.watchWindow.refresh()
+                # Output to the log file
+                self.outputToTraceLog('%s = %s\n' % (var, val))
+                self.debug('%s = %s\n' % (var, val))
+
+    def updateMVar(self, machine, chTypeString, chAddress):
+        # Called when a memory location changes to update any
+        # watched M-variable that points to the address.
+        if machine == self.activeMachine:
+            for var,text in self.watchVars.iteritems():
+                if var[0] == 'm':
+                    addr = machine.mVariables[int(var[1:])]
+                    typeString, address, offset, size, formatString = \
+                        machine.decodeMVarAddress(addr)
+                    if typeString == chTypeString and address == chAddress:
+                        valid, val = machine.getMemory(addr)
+                        # Write the watch line
+                        maxY, maxX = self.watchWindow.getmaxyx()
+                        line = self.watchVars[var]
+                        self.watchWindow.hline(line,0, ' ', maxX)
+                        self.watchWindow.addstr(line,0, '%s=%s' % (var, val))
+                        self.watchWindow.refresh()
+                        # Output to the log file
+                        self.outputToTraceLog('%s = %s\n' % (var, val))
+                        self.debug('%s = %s\n' % (var, val))
+
+    def unwatchVar(self, machine, var):
+        if machine == self.activeMachine:
+            if var in self.watchVars:
+                maxY, maxX = self.watchWindow.getmaxyx()
+                line = self.watchVars[var]
+                self.watchWindow.hline(line,0, ' ', maxX)
+                self.watchWindow.refresh()
+                del self.watchVars[var]
+
+    def setActiveMachine(self, machine):
+        self.activeMachine = machine
+
---
+++ etc/simulations/PmacUser.py
@@ -0,0 +1,164 @@
+from pkg_resources import require
+require('dls_simulationlib')
+#require('dls_motorcontrol')
+import dls_simulationlib.simsocket
+import pmcpreprocessor
+from PmacParser import *
+from PmacUi import *
+import os, sys, math
+
+class FileUser(PmacParser):
+    def __init__(self, fileName, pmac, includePaths):
+        getUi().output("Reading PMC file %s\n" % fileName)
+        PmacParser.__init__(self, pmac)
+        p = pmcpreprocessor.clsPmacParser(includePaths = includePaths)
+        converted = p.parse(fileName, debug=True)
+        if converted is not None:
+            for line in p.output:
+                self.process(line, True)
+
+    def reply(self, data):
+        pass
+
+class ConsoleUser(PmacParser):
+    def __init__(self, pmac, prompt):
+        PmacParser.__init__(self, pmac)
+        self.prompt = prompt
+
+    def reply(self, data):
+        text = data.rstrip()
+        if len(text) > 0:
+            getUi().output("%s\n" % text)
+
+    def doConsole(self, machine):
+        going = True
+        getUi().setActiveMachine(machine)
+        while going:
+            text = getUi().inputLine('%s> ' % self.prompt)
+            getUi().output('%s> %s\n' % (self.prompt, text.rstrip()))
+            if text.strip().lower() == 'quit':
+                going = False
+            else:
+                self.process(text + '\n', True)
+        getUi().setActiveMachine(None)
+
+class TcpUser(dls_simulationlib.simsocket.dataSocket, PmacParser):
+    def __init__(self, clientSocket, pmac):
+        dls_simulationlib.simsocket.dataSocket.__init__(self, clientSocket)
+        PmacParser.__init__(self, pmac)
+        self.tcpInputData = ''
+        self.tcpOutputData = ''
+
+    def receive(self, data):
+        '''Override to receive data from the socket'''
+        self.tcpInputData += data
+        command, self.tcpInputData = self.findPacket(self.tcpInputData)
+        while len(command) > 0:
+            if len(command) >= 8:
+                if command[1] == '\xbf':   #Get response
+                    dataSize = ord(command[6]) * 256 + ord(command[7])
+                    if dataSize > 0 and dataSize <= (len(command)-8):
+                        self.process(command[8:8+dataSize])
+                        self.transmit(self.getOutputData())
+                elif command[1] == '\xb3':   #Flush
+                    self.transmit(self.processPmacFlush())
+                elif command[1] == '\xc2':   #Read ready
+                    self.transmit(self.processReadReady())
+            command, self.tcpInputData = self.findPacket(self.tcpInputData)
+
+    def findPacket(self, data):
+        '''Looks for a complete packet in the input stream. Returns a
+           tuple containing the command packet followed by any remaining data.'''
+        command = ""
+        remaining = ""
+        found = False
+        dataSize = 0
+        state = "searching"
+        for ch in data:
+            if state == "searching":
+                if ch == '\x40' or ch == '\xc0':
+                    command += ch
+                    state = "header"
+            elif state == "header":
+                command += ch
+                if len(command) == 7:
+                    dataSize = ord(ch) * 256
+                elif len(command) == 8:
+                    dataSize += ord(ch)
+                    if dataSize == 0:
+                        state = "remaining"
+                        found = True
+                    else:
+                        state = "data"
+            elif state == "data":
+                command += ch
+                if (len(command) - 8) >= dataSize:
+                    state = "remaining"
+                    found = True
+            elif state == "remaining":
+                remaining += ch
+        if found:
+            return (command, remaining)
+        else:
+            return ('', command)
+
+    def getOutputData(self):
+        '''Returns the next block of data to return to the host.  The data
+        is selected according to the manual.'''
+        result = ""
+        stopOnCr = False
+        for ch in self.tcpOutputData:
+            result += ch
+            if ch == '\x06' or ch == '\n':
+                break
+            elif ch == '\x07' or ch == '\x02':
+                stopOnCr = True
+            elif ch == '\r' and stopOnCr:
+                break
+            elif len(result) >= 1440:
+                break
+        self.tcpOutputData = self.tcpOutputData[len(result):]
+        if len(result) > 0:
+            result += '\x06'
+        return result
+    
+    def disconnectInd(self):
+        '''Override to receive disconnect indications for the socket.'''
+        #self.xps.userClosed(self)
+
+    def reply(self, data):
+        self.tcpOutputData += data
+
+    def processPmacFlush(self):
+        '''Processes a PMAC flush command by reinitialising the
+        input state of the controller. The manual states one character
+        is returned but does not indicate the value.  The support does
+        not check the value.'''
+        self.cmdState = "none"
+        self.cmdParameter = ""
+        self.cmdAddress = ""
+        self.tcpOutputData = ""
+        return '\x40'
+
+    def processReadReady(self):
+        '''Returns a string consisting of 1 character which is NULL if there
+        is no data ready to read.  Two characters are returned, the first
+        is zero for no data to read.  The second is unspecified.'''
+        if len(self.tcpOutputData) == 0:
+            return '\x00\x40'
+        else:
+            return '\x01\x40'
+
+class TcpUserServer(dls_simulationlib.simsocket.listenSocket):
+    def __init__(self, pmac, tcpPort):
+        dls_simulationlib.simsocket.listenSocket.__init__(self)
+        self.pmac = pmac
+        self.tcpPort = tcpPort
+        self.createTcpServer(self.tcpPort)
+
+    def createClient(self, clientSocket):
+        '''Override to create a client handler object.'''
+        getUi().output("Connecting PMAC user on port %s.\n" % self.tcpPort)
+        return TcpUser(clientSocket, self.pmac)
+
+
